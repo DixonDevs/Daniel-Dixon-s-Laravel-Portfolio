0042161cb03bd8c94173ebc70c48fdd5352343da
Daniel <ddixon@aceonetechnologies.com>
2024-05-30 13:02:24 -0500

    Updates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,15 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Unbounded:wght@600&display=swap">
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     <title>Home Page</title>
 </head>
 <body>
+<div class="wrapper">
     @include('partials.header')
     <div class="full-screen-section">
         <h1>HELLO</h1>
-        <p>I AM A SOFTWARE DEVELOPER</p>
+        <p class="under-hello">I AM A SOFTWARE DEVELOPER</p>
+        <button class="portfolio-button">See Portfolio</button>
     </div>
+
+    <div class="full-screen-section-2">
+        <div class="content-wrapper">
+            <img class="daniel-image" src="images\8bit image of Daniel.jpg" alt="8-bit Character of Daniel Dixon" width="600" height="650">
+            <button class="portfolio-button">See Portfolio</button>
+        </div>
+    </div>
+</div>
 </body>
 </html>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,6 @@
 <div class="sticky-header-wrapper">
     <div class="sticky-header">
+        <h1 class="name-logo">DanielDixon.dev </h1> <!-- Adding the .name-logo here -->
         <ul class="nav-links">
             <li><a href="{{ route('home') }}">HOME</a></li>
             <li><a href="{{ route('about') }}">ABOUT</a></li>
