6ea2111d78a999301e757377418285717930f463
DixonDevs <danieldixon.dev@gmail.com>
2024-06-21 10:42:25 -0500

    fix: vit.config.js laravel input

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [
     laravel({
-      input: 'resources\css\app.css',
+      input: 'public\\css\\styles.css',
       refresh: true,
     }),
-    react(),
   ],
 });
