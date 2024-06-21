77e005a1392ee038c898fc858475aa23b14151d4
DixonDevs <danieldixon.dev@gmail.com>
2024-06-21 10:46:01 -0500

    fix: vit.config.js laravel input agian

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: 'public\\css\\styles.css',
-      refresh: true,
-    }),
-  ],
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css'],
+            refresh: true,
+        }),
+    ],
 });
